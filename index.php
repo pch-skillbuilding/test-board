59fc3fc6009fa6730f2e8e1ecf688d2284610a30
Ustat Singh <usingh@pch.com>
2013-06-18 17:49:42 -0400

    Coming soon page initial checkin

---
+++ index.php
@@ -0,0 +1,29 @@
+
+<html>
+ <head>
+  <title>Test-Board</title>
+	<style>
+		.content {
+			width:400px;
+			margin: 100px auto;
+			text-align: center;		
+}
+
+		p {
+			font-weight:bold;
+			font-size: 60px;
+		}
+		body
+{
+background-image: url(images/bgimage.jpg);
+background-repeat: no-repeat;
+background-color: sandybrown;
+}
+	</style>
+ </head>
+ <body>
+	<div class="content">
+	 <p>Coming Soon</p>
+	</div>
+ </body>
+</html>
